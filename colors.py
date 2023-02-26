e5b148570ca5a2514f4841b3df65da1998163ac6
raweber <raweber@student.42wolfsburg.de>
2023-02-26 19:24:26 +0100

    got it

---
+++ colors.py
@@ -0,0 +1,6 @@
+class colors:
+	BLUE = '\033[94m'
+	CYAN = '\033[96m'
+	GREEN = '\033[92m'
+	RED = '\033[0;31m'
+	END = '\033[0m' 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+from colors import colors
 
 def estimate_price(mileage, m_now, b_now):
 	return (b_now + (m_now * mileage))
@@ -30,68 +32,82 @@ def gradient_descent(m_now, b_now, data, L):
 		# m_gradient += np.sum((estimate_price(x, b_now, m_now) - y) * x)
 		# b_gradient += L * (1/n) * estimate_price(x, b_now, m_now) - y
 		# m_gradient += L * (1/n) * (estimate_price(x, b_now, m_now) - y) * x
-		b_gradient += estimate_price(x, m_now, b_now) - y
-		m_gradient += (estimate_price(x, m_now, b_now) - y) * x
+		m_gradient = (estimate_price(x, m_now, b_now) - y) * x
+		b_gradient = estimate_price(x, m_now, b_now) - y
+		m_now -= L * (1 / n) * m_gradient
+		b_now -= L * (1 / n) * b_gradient
 
-	b_gradient /= n;
-	m_gradient /= n;
-	m = m_gradient * L
-	b = b_gradient * L
+	# b_gradient /= n;
+	# m_gradient /= n;
+	# m = m_gradient * L
+	# b = b_gradient * L
 
-	return (m, b)
+	return (m_now, b_now)
 
+
+# minimum step size: 0.001 !!! for m_gradient * L
+# maximum number of steps: 1000
 
 def main():
 	data = pd.read_csv('data.csv')
 	m = 0
 	b = 0
-	L = 0.0001
-	epochs = 100
-	print("#Estimate price BEFORE training (500 miles): ", estimate_price(500, m, b))
+	L = 0.1
+	epochs = 1000
+	while 42:
+		try:
+			input_mileage = int(input(f"{colors().BLUE}Which mileage do you want to have a price estimation for? {colors().END}"))
+			if input_mileage < 0:
+				print(f"{colors().RED}Invalid input, please enter a positive integer value!{colors().END}")
+				continue
+			break
+		except:
+			print(f"{colors().RED}Invalid input, please enter a positive integer value!{colors().END}")
+
 
 	########### normalize data start ###########
+	print("\nNormalizing data from data.csv...")
+	time.sleep(1)
 	max_km = data['km'].max()
-	min_km = data['km'].min()
 	max_price = data['price'].max()
-	min_price = data['price'].min()
-	data['km'] = data['km'].astype(float)
-	data['price'] = data['price'].astype(float)
-	for i in range(len(data)):
-		# data.iloc[i, data.columns.get_loc('km')] = data.iloc[i, data.columns.get_loc('km')] / max_km 
-		# data.iloc[i, data.columns.get_loc('price')] = data.iloc[i, data.columns.get_loc('price')] / max_price
-		data.iloc[i, data.columns.get_loc('km')] = (data.iloc[i, data.columns.get_loc('km')] - min_km) / (max_km - min_km) 
-		data.iloc[i, data.columns.get_loc('price')] = (data.iloc[i, data.columns.get_loc('price')] - min_price) / (max_price - min_price) 
-	# print(data)
+	data['km'] /= max_km
+	data['price'] /= max_price
 	########### normalize data end ###########
 
 
-	for i in range(epochs):
+	########### training the model start ###########
+	print("\nTraining the model...")
+	for i in range(1, epochs + 1):
 		m, b = gradient_descent(m, b, data, L)
-		# print(m, b)
-	# print("loss function returns: ", loss_function(m, b, data))
-
-	############ DEnormalize data start ###########
-	# for i in range(len(data)):
-	# 	data.iloc[i, data.columns.get_loc('km')] = (data.iloc[i, data.columns.get_loc('km')] * (max_km - min_km) + min_km)
-	# 	data.iloc[i, data.columns.get_loc('price')] = (data.iloc[i, data.columns.get_loc('price')] * (max_price - min_price) + min_price)
-	# print(data)
-	############ DEnormalize data end ###########
+		if (i % 100) == 0:
+			print(f"Loss function after {i} of {epochs} epochs returns: ", loss_function(m, b, data))
+	
+	print(f"{colors().GREEN}Final loss function result after {i} epochs is: ", loss_function(m, b, data), colors().END)
+	########### training the model end ###########
 
 
-	print("#Estimate price AFTER training (500 miles): ", estimate_price(500, m, b))
+	############ denormalize data start ###########
+	print("\nDenormalizing data...")
+	data['km'] *= max_km
+	data['price'] *= max_price
+	b *= max_price
+	m *= (max_price / max_km)
+	############ denormalize data end ###########
+
+
+	estimate = round(estimate_price(input_mileage, m, b))
+	if estimate < 0:
+		estimate = 0
+	print(f"\n{colors().BLUE}ESTIMATED PRICE: ${estimate}{colors().END}\n")
 	plt.scatter(data.km, data.price, color="black")
 	# Plot the resulting regression line
-    # x = data["km"]
-    # y = (theta1 * x) + theta0 # y = mx + b 
-    # plt.plot(x, y, 'r')
-    # plt.scatter(data.km, data.price)
 	plt.xlabel('mileage')
 	plt.ylabel('price')
 	plt.plot(
-		list(range(0, 2)), 
-		[m * x + b for x in range(0, 2)], 
+		list(range(0, max_km)), 
+		[m * x + b for x in range(0, max_km)], 
 		color="green")
-
+	plt.plot(input_mileage, estimate, marker="o", markersize=10, markerfacecolor="lightblue")
 	plt.show()
 
 
